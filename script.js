1f8934f928b434a0c5bd62eafe923fd68613be6f
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-05-26 12:08:50 +0200

    add button function

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const bookmarkIcon = document.querySelector('[data-js="bookmarkIcon"');
-
-function toggleBookmarkIcon() {
-  bookmarkIcon.classList.toggle("card__bookmark--flagged");
-}
+const answerButton = document.querySelector('[data-js="answerButton"');
+const answerSection = document.querySelector('[data-js="answerSection"');
 
 bookmarkIcon.addEventListener("click", () => {
-  toggleBookmarkIcon();
+  bookmarkIcon.classList.toggle("card__bookmark--flagged");
+});
+
+answerButton.addEventListener("click", () => {
+  answerButton.classList.toggle("button--show-answer");
+
+  console.log(answerButton.getAttribute("hidden"));
+
+  //   if (answerButton.textContent === "Show Answer") {
+  //     answerButton.textContent = "Hide Answer";
+  //   } else {
+  //     answerButton.textContent = "Show Answer";
+  //   }
+
+  //   answerButton.classList === "button--show-answer"
+  //     ? (answerButton.textContent = "Show Answer")
+  //     : (answerButton.textContent = "Hide Answer");
+
+  answerSection.toggleAttribute("hidden");
 });
