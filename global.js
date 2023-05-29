65c32d5241b834e0107c985f3b34445e411a8c92
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-05-29 17:05:22 +0200

    add icons

--- global.js
+++ global.js
@@ -1,9 +1,7 @@
 function registerServiceWorker() {
-  console.log("registered");
   navigator.serviceWorker.register("/serviceworker.js");
 }
 
 if ("serviceWorker" in navigator) {
-  console.log("check");
   registerServiceWorker();
 }
